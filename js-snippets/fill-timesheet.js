1c6393614cd34ad856fb4e944b60fc1ae0ae6e60
Alex Wohlbruck <alex.wohlbruck@xenial.com>
2023-01-06 20:00:48 -0500

    Fix 24 hour conversion bug

--- js-snippets/fill-timesheet.js
+++ js-snippets/fill-timesheet.js
@@ -2,7 +2,7 @@
 const convertTo24Hour = (time) => {
   const [hours, minutesPeriod] = time.split(':')
   const [minutes, period] = minutesPeriod.split(/([A-Z]+)/)
-  const hours24 = (period === 'AM' || hours === '12') ? hours : (parseInt(hours) + 12).toString()
+  const hours24 = (period === 'AM' && hours !== '12') ? hours : (parseInt(hours) + 12).toString()
 
   // Pad hours and minutes with 0 if needed
   return `${hours24.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`
